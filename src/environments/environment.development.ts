085618dbe4b0caa272b3808602ec26554ee4c389
alexandre-jme1234 <109076155+Dev2socializus@users.noreply.github.com>
2024-03-14 18:25:36 +0100

    lkj

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    key_TMDB: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZTNmYmYyMjg5ZjQ4NmM3ZGE3NWQyMTdiN2U2MzU4NCIsInN1YiI6IjY0M2ZiMTFmYjBiYTdlMDUyNzEzMTQ3NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TPm14QDpLKeSD-nFW16fzxQap0RSELJKPZ4fcJBQKPM',
-    url_TMDB: 'https://api.themoviedb.org/3/discover/movie?primary_release_date.lte=2024-01&region=fr-FR&primary_release_year=2024&sort_by',
-    url_local: 'http://localhost:1337/api/',
-    key_STRAPI: 'Bearer 87dd7e32a413d376d3f517db826ac13a5a72533019fd712a445fa30078bfbb933b00f38e9eae316671d55c90c0c05904c0e6f278a2142c36160a54de3445ead6d8cc0c40a9d969448c5355e0ce704c1060c4c5eb2a93b3eaf9d29f61b44a1b72008c61367e5cade7857ae0fe7eb28bf0dfdf5d540bfabe28695f01fe3ad822ff'
-};

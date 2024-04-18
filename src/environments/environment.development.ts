60658036e759793022f66b35caba249acdf50ced
alexandre-jme1234 <109076155+Dev2socializus@users.noreply.github.com>
2024-04-18 13:11:31 +0200

    save env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    url_local: 'XXX-XXX-XXX',
-    url_TMDB: 'XXX-XXX-XXX',
-    key_TMDB: 'XXX-XXX-XXX',
+    url_local: 'http://localhost:1337/api/moovies',
+    url_TMDB: 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&page=1&sort_by=primary_release_date.desc&year=2023',
+    key_TMDB: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZTNmYmYyMjg5ZjQ4NmM3ZGE3NWQyMTdiN2U2MzU4NCIsInN1YiI6IjY0M2ZiMTFmYjBiYTdlMDUyNzEzMTQ3NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TPm14QDpLKeSD-nFW16fzxQap0RSELJKPZ4fcJBQKPM',
     key_STRAPI: 'XXX-XXX-XXX'
 };

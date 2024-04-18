f89a912ad301e0414d523f3a396300b6037b7a27
alexandre-jme1234 <109076155+Dev2socializus@users.noreply.github.com>
2024-04-18 14:23:17 +0200

    config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
     url_local: 'http://localhost:1337/api/moovies',
     url_TMDB: 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&page=1&sort_by=primary_release_date.desc&year=2023',
     key_TMDB: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZTNmYmYyMjg5ZjQ4NmM3ZGE3NWQyMTdiN2U2MzU4NCIsInN1YiI6IjY0M2ZiMTFmYjBiYTdlMDUyNzEzMTQ3NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TPm14QDpLKeSD-nFW16fzxQap0RSELJKPZ4fcJBQKPM',
-    key_STRAPI: 'XXX-XXX-XXX'
+    key_STRAPI: 'Bearer a91b1509373829a0243982e8199e5022bf4c95fa708441ec5b9060a9079c0c0b1d1f7cd591e759114638a71469919998594e0e8a1da7f2f8948671a88e0eef5fccf43d17868d1e710fe19b9ccbc982407b557cf70ddc2c8902fb7edcf7818e60a19772c675b7a1417a8e59e22fdd48d8558cefc76607e454b7b3902f18f1fa04'
 };
